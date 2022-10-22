219aaca7da939b243fa505055aeaa77fd60fbec9
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2022-10-22 19:26:24 +0300

    Added carousel on homepage

--- NASCAR_Frontend/src/Components/Pages/HomePage.js
+++ NASCAR_Frontend/src/Components/Pages/HomePage.js
@@ -1,10 +1,14 @@
+import BaseComponent from 'bootstrap/js/dist/base-component';
 import React, { Component } from 'react';
-
+import { UncontrolledCarousel, Carousel, CarouselItem } from 'reactstrap';
+//import { Carousel } from 'react-responsive-carousel';
 export class HomePage extends Component {
     static displayName = HomePage.name;
 
     constructor(props) {
         super(props);
+        this.WelcomeCarousel = this.WelcomeCarousel.bind(this);
+        this.renderHomePage = this.renderHomePage.bind(this);
         this.state = { top5Pilots: [], loading: true };
     }
 
@@ -12,10 +16,50 @@ export class HomePage extends Component {
         this.populateHome();
     }
 
-    static renderHomePage(top5Pilots) {
+    WelcomeCarousel(){
+        return(<div>
+        <UncontrolledCarousel
+                items={[
+                {
+                    className:'welcome-carousel',
+                    altText: 'Slide 1',
+                    key: 1,
+                    src: '\\Images\\WelcomeCarousel_1.jpg',
+                },
+                {
+                    className:'welcome-carousel',
+                    altText: 'Slide 2',
+                    key: 2,
+                    src: '\\Images\\WelcomeCarousel_2.jpg'
+                },
+                {
+                    className:'welcome-carousel',
+                    altText: 'Slide 3',
+                    key: 3,
+                    src: '\\Images\\WelcomeCarousel_3.jpg'
+                },
+                {
+                    className:'welcome-carousel',
+                    altText: 'Slide 4',
+                    key: 4,
+                    src: '\\Images\\WelcomeCarousel_4.jpg'
+                }]} controls={false} indicators={false} autoPlay={true} interval='5000'/></div>);
+    }
+
+
+    renderHomePage(top5Pilots) {
         return (
-            <div><h1>Welcome</h1></div>
-        );
+            <div className='d-flex justify-content-center carDiv'>
+                <div className='position-relative'>
+                    <this.WelcomeCarousel/>
+                </div>
+                <div className='position-absolute'>
+                    <h1 className='text-c text-dark'>Добро пожаловать в NASCAR</h1>
+                </div>
+            </div>
+
+            
+        )            
     }
 
     render() {
@@ -23,12 +67,10 @@ export class HomePage extends Component {
             ? <div className='d-flex justify-content-center my-auto'>
                 <p><em>Loading... please wait.</em></p>
               </div>
-            : HomePage.renderHomePage(this.state.top5Pilots);
-
+            : <this.renderHomePage top5Pilots={this.state.top5Pilots}/>;
+        
         return (
-            <div>
-                <h1 id="tabelLabel" >Weather forecast</h1>
-                <p>This component demonstrates fetching data from the server.</p>
+            <div className='d-flex justify-content-center'>
                 {contents}
             </div>
         );
@@ -38,6 +80,6 @@ export class HomePage extends Component {
         const response = await fetch('api');
         console.log(response);
         const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
+        this.setState({ top5Pilots: data, loading: false });
     }
 }
